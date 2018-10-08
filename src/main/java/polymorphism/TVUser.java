9a5d05ffb3c381723236dbd634b596c39eb9deca
circleGiven <circlegiven@gmail.com>
2018-10-08 19:42:22 +0900

    day 1
    - <bean> 엘리먼트 속성
    - scope (singleton)

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -8,11 +8,14 @@ public class TVUser {
         // 1. Spring 컨테이너 구동
         AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
         // 2. spring 컨테이너로 부터 필요한 객체를 요청
-        TV tv = (TV)factory.getBean("tv");
-        tv.powerOn();
-        tv.volumnUp();
-        tv.volumnDown();
-        tv.powerOff();
+        TV tv1 = (TV)factory.getBean("tv");
+        TV tv2 = (TV)factory.getBean("tv");
+        TV tv3 = (TV)factory.getBean("tv");
+
+//        tv.powerOn();
+//        tv.volumnUp();
+//        tv.volumnDown();
+//        tv.powerOff();
         // 3. 스프링 컨테이너 종료
         factory.close();
     }
