c50b040b6b714510fb6e311aa15ca758b1bf975b
circleGiven <circlegiven@gmail.com>
2018-10-08 19:57:37 +0900

    day 1
    - 의존성 주입
    - 생성자 인젝션
    - 의존관계 변경

--- src/main/java/polymorphism/SonySpeaker.java
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -1,16 +1,16 @@
 package polymorphism;
 
-public class SonySpeaker {
+public class SonySpeaker implements Speaker {
 
     public SonySpeaker() {
         System.out.println("===> SonySpeaker 객체 생성");
     }
 
-    public void volumnUp() {
+    public void volumeUp() {
         System.out.println("SonySpeaker---소리 올린다.");
     }
 
-    public void volumnDown() {
+    public void volumeDown() {
         System.out.println("SonySpeaker---소리 내린다.");
     }
 }
