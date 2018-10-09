9307a2538ba80cdfefbb7a61a308aaddaf2aca3c
circleGiven <circlegiven@gmail.com>
2018-10-09 14:19:36 +0900

    day 1
    - 비즈니스 컴포넌트
    - Service 구현

---
+++ src/main/java/biz/board/BoardService.java
@@ -0,0 +1,13 @@
+package biz.board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    // CRUD
+    void insertBoard(BoardVO vo);
+    void updateBoard(BoardVO vo);
+    void deleteBoard(BoardVO vo);
+    BoardVO getBoard(BoardVO vo);
+    List<BoardVO> getBoardList(BoardVO vo);
+}
