f5ee82cdc238377c941a1475e3632af35f51aa5b
circleGiven <circlegiven@gmail.com>
2018-10-13 15:16:49 +0900

    Day 2
    - AOP
    - JoinPoint와 바인드 변수
    - Before

--- src/main/java/biz/common/BeforeAdvice.java
+++ src/main/java/biz/common/BeforeAdvice.java
@@ -1,7 +1,11 @@
 package biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class BeforeAdvice {
-    public void beforeLog() {
-        System.out.println("[사전처리] 비즈니스 로직 수행 전 동작");
+    public void beforeLog(JoinPoint point) {
+        String method = point.getSignature().getName();
+        Object[] args = point.getArgs();
+        System.out.println("[사전처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
     }
 }
