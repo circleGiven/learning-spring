c202f589e65eb03f1b7fd7fae38e5be40f52804a
circleGiven <circlegiven@gmail.com>
2018-10-13 16:15:26 +0900

    Day 2
    - AOP
    - 어노테이션 사용
    - pointcut 설정

--- src/main/java/biz/common/LogAdvice.java
+++ src/main/java/biz/common/LogAdvice.java
@@ -1,10 +1,15 @@
 package biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LogAdvice {
 
-    public void printLog(JoinPoint point) {
-        System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
-    }
+    @Pointcut("execution(* biz..*Impl.*(..))")
+    public void allPointcut() {}
+
+    @Pointcut("execution(* biz..*Impl.get*(..))")
+    public void getPointcut() {}
 }
