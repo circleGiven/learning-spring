7d4d476c3b5bdc0db3a20dcf7244eee08561674b
circleGiven <circlegiven@gmail.com>
2018-10-13 16:32:02 +0900

    Day 2
    - AOP
    - 어노테이션 사용
    - @Before 어드바이스

--- src/main/java/biz/common/BeforeAdvice.java
+++ src/main/java/biz/common/BeforeAdvice.java
@@ -1,11 +1,22 @@
 package biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class BeforeAdvice {
+
+    @Pointcut("execution(* biz..*Impl.*(..))")
+    public void allPointcut() {}
+
+    @Before("allPointcut()")
     public void beforeLog(JoinPoint point) {
         String method = point.getSignature().getName();
         Object[] args = point.getArgs();
-        System.out.println("[사전처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
+        System.out.println("[사전 처리] " + method + "() 메소드 ARGS 정보 : " + args[0].toString());
     }
 }
