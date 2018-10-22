d65bef5604bc555479f3605d759e3b6673e531ca
circleGiven <circlegiven@gmail.com>
2018-10-22 22:58:55 +0900

    Day 4 @RequestParam 추가

--- src/main/java/view/board/BoardController.java
+++ src/main/java/view/board/BoardController.java
@@ -5,6 +5,7 @@ import biz.board.impl.BoardDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -52,8 +53,13 @@ public class BoardController {
 
     // 글 목록 검색
     @RequestMapping("/getBoardList.do")
-    public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+    public String getBoardList(@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String condition,
+                               @RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword,
+                               BoardVO vo, BoardDAO boardDAO, Model model) {
         System.out.println("글 목록 검색 처리");
+        System.out.println("검색 조건 : " + condition);
+        System.out.println("검색 단어 : " + keyword);
+
         // 검색 결과 저장
         model.addAttribute("boardList", boardDAO.getBoardList(vo));
         // 화면 정보 설정
