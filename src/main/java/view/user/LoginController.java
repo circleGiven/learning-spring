bac0cb519228f75d327125a80f28b295f02189d1
circleGiven <circlegiven@gmail.com>
2018-10-22 18:02:50 +0900

    Day 4 JSP 에서 command 객체 사용
    - 테스트를 위한 index.jsp 추가

--- src/main/java/view/user/LoginController.java
+++ src/main/java/view/user/LoginController.java
@@ -4,13 +4,22 @@ import biz.user.UserVO;
 import biz.user.impl.UserDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login.do")
+    @RequestMapping(value = "/login.do", method = RequestMethod.GET)
+    public String loginView(UserVO vo) {
+        System.out.println("로그인 화면으로 이동");
+        vo.setId("admin");
+        vo.setPassword("admin");
+        return "login.jsp";
+    }
+
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST)
     public String login(UserVO vo, UserDAO userDAO) {
-        System.out.println("로그인 처리");
+        System.out.println("로그인 인증 처리...");
 
         // 화면 네비게이션
         if (null != userDAO.getUser(vo)) {
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: choewonjun
+  Date: 22/10/2018
+  Time: 5:58 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Main Page</title>
+</head>
+<body>
+    <center>
+        <h1>게시판 프로그램</h1>
+        <hr>
+        <a href="login.do">로그인</a><br><br><br>
+        <a href="getBoardList.do">글 목록 바로가기</a>
+        <hr>
+    </center>
+</body>
+</html>
