9c413d69e4c80a5a7647981ea68dd6cbbeb46ba8
circleGiven <circlegiven@gmail.com>
2018-10-22 11:36:08 +0900

    Day 3 DispatcherServlet 설정파일 수정 - LoginController 스프링에서 제공하는 Controller로 구현작업

--- src/main/java/view/user/LoginController.java
+++ src/main/java/view/user/LoginController.java
@@ -2,7 +2,8 @@ package view.user;
 
 import biz.user.UserVO;
 import biz.user.impl.UserDAO;
-import view.controller.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController implements Controller {
 
     @Override
-    public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("로그인 처리");
 
         // 사용자 입력 정보 추출
@@ -26,10 +27,12 @@ public class LoginController implements Controller {
         UserVO user = userDAO.getUser(vo);
 
         // 화면 네비게이션
+        ModelAndView mav = new ModelAndView();
         if (null != user) {
-            return "getBoardList.do";
+            mav.setViewName("getBoardList.do");
         } else {
-            return "login";
+            mav.setViewName("login.jsp");
         }
+        return mav;
     }
 }
